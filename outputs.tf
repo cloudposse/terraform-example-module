3a6464e9e513c6556c6bdc99476e4ebb4403d047
Nuru <Nuru@users.noreply.github.com>
2020-08-29 18:14:51 -0700

    Convert to `context.tf` (#5)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
+output "id" {
+  description = "ID of the created example"
+  value       = module.this.enabled ? module.this.id : null
+}
+
 output "example" {
   description = "Example output"
-  value       = local.example
+  value       = module.this.enabled ? local.example : null
+}
+
+output "random" {
+  description = "Stable random number for this example"
+  value       = module.this.enabled ? join("", random_integer.example[*].result) : null
 }
