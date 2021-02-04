42706d7c64b27af410998ee841a0b768882374d2
Nuru <Nuru@users.noreply.github.com>
2021-02-03 19:54:36 -0800

    Update to current workflows and context.tf 0.23.0

--- versions.tf
+++ versions.tf
@@ -1,14 +1,11 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.13"
 
   required_providers {
+    # Update these to reflect the actual requirements of your module
     local = {
       source  = "hashicorp/local"
       version = ">= 1.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.2"
     }
   }
 }
