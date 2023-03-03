846271074a3444119201d237970e11bc029fb9c9
Max Lobur <max_lobur@outlook.com>
2023-03-03 15:15:57 +0200

    Example terraform fmt

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -4,11 +4,9 @@ output "id" {
 }
 
 output "example" {
-  description = "Output \"example\" from example module"
   value       = module.example.example
 }
 
 output "random" {
-  description = "Output \"random\" from example module"
   value       = module.example.random
 }
