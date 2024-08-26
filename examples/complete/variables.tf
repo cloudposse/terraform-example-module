71c8b0473880833db8bfdee024d355cdc61d0057
Igor Rodionov <goruha@gmail.com>
2024-08-26 21:09:58 +0200

    Test terratest

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-  type        = string
-  description = "The AWS region in which the resources will be created"
-}
-
 variable "example_input" {
   type        = string
   description = "The value which will be passed to the example module"
